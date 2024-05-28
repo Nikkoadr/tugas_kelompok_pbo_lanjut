5b3390936974f0248d5c13ac1277f8c4446cf41f
Nikko Adrian <nikkoadrian02@gmail.com>
2024-05-28 14:17:53 +0700

    edit model

--- modules/data_alumni/Data_alumniModel.py
+++ modules/data_alumni/Data_alumniModel.py
@@ -10,13 +10,14 @@ class Data_alumniModel(CoreModel):
         connection = get_db()
         cursor = connection.cursor()
         query = """
-        SELECT alumni.id_lulus, alumni.tgl_yudisium, alumni.no_ijazah, alumni.judul_skripsi, 
-            alumni.ipk_lulus, alumni.id_wisuda, alumni.id_mahasiswa, alumni.bidang_kerja, 
-            alumni.perusahaan_kerja, user.nama_awal, user.nama_akhir
-        FROM alumni
-        JOIN mahasiswa ON alumni.id_mahasiswa = mahasiswa.id_mahasiswa
-        JOIN user ON mahasiswa.id_user = user.id_user;
-        """
+            SELECT alumni.id_lulus, alumni.tgl_yudisium, alumni.no_ijazah, alumni.judul_skripsi, 
+                alumni.ipk_lulus, alumni.id_wisuda, alumni.id_mahasiswa, alumni.bidang_kerja, 
+                alumni.perusahaan_kerja, user.nama_awal, user.nama_akhir, wisuda.tgl_wisuda
+            FROM alumni
+            JOIN mahasiswa ON alumni.id_mahasiswa = mahasiswa.id_mahasiswa
+            JOIN user ON mahasiswa.id_user = user.id_user
+            JOIN wisuda ON alumni.id_wisuda = wisuda.id_wisuda;
+            """
         cursor.execute(query)
         result = cursor.fetchall()
         cursor.close()
