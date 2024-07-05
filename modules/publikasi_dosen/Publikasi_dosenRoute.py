5fccb5dd381cc56ea5d468fb99a3e8cd1f925b58
Nikko Adrian <nikkoadrian02@gmail.com>
2024-07-05 11:13:53 +0700

    update

--- modules/publikasi_dosen/Publikasi_dosenRoute.py
+++ modules/publikasi_dosen/Publikasi_dosenRoute.py
@@ -3,9 +3,8 @@ from modules.publikasi_dosen.Publikasi_dosenView import *
 
 app_publikasi_dosen = Blueprint('app_publikasi_dosen',__name__, template_folder='templates')
 app_publikasi_dosen.add_url_rule('/','index', Publikasi_dosenView().index, methods=['GET'])
-app_publikasi_dosen.add_url_rule('/dosen','dosen', Publikasi_dosenView().dosen, methods=['GET'])
-app_publikasi_dosen.add_url_rule('/create','create',Publikasi_dosenView().create, methods=['POST'])
 app_publikasi_dosen.add_url_rule('edit/<int:id>','edit', Publikasi_dosenView().edit, methods=['GET'])
 app_publikasi_dosen.add_url_rule('/store','store', Publikasi_dosenView().store, methods=['POST'])
 app_publikasi_dosen.add_url_rule('/update/<int:id>', 'update', Publikasi_dosenView().update, methods=['POST'])
 app_publikasi_dosen.add_url_rule('delete/<int:id>','delete', Publikasi_dosenView().delete, methods=['GET'])
+app_publikasi_dosen.add_url_rule('/publish_jurnal','publish_jurnal', Publikasi_dosenView().publish_jurnal, methods=['GET'])
--- modules/publikasi_dosen/Publikasi_dosenView.py
+++ modules/publikasi_dosen/Publikasi_dosenView.py
@@ -7,28 +7,21 @@ class Publikasi_dosenView:
     def index():
         data = Publikasi_dosenModel().publikasi_dosen_all()
         return render_template('publikasi_dosen_index.html',data=data)
-    
-    @staticmethod
-    def dosen():
-        data = Publikasi_dosenModel().get_dosen()
-        return render_template('dosen.html', data=data)
 
     @staticmethod
-    def create():
-        id_dosen = request.form['id_dosen']
-        nidn = request.form['nidn']
-        nama_awal = request.form['nama_awal']
-        nama_akhir = request.form['nama_akhir']
-        return render_template('publikasi_dosen_create.html', id_dosen=id_dosen, nidn=nidn, nama_awal=nama_awal, nama_akhir=nama_akhir)
+    def publish_jurnal():
+        return render_template('publikasi_dosen_create.html')
     
     @staticmethod
     def store():
         obj = Publikasi_dosenModel()
-        post = request.form
-        obj.tgl_publikasi = post['tgl_publikasi']
-        obj.judul = post['judul']
-        obj.jenis_publikasi = post['jenis_publikasi']
-        obj.id_dosen = post['id_dosen']
+        form = request.form
+        obj = Publikasi_dosenModel()
+        obj.judul = form['judul']
+        obj.jenis = form['jenis']
+        obj.tahun = form['tahun']
+        obj.abstrak = form['abstrak']
+        obj.status = 'review'
         Publikasi_dosenModel().store(obj)
         flash('Data Berhasil Ditambahkan', 'success')
         return redirect('/publikasi_dosen')
